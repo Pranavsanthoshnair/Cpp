6842df760223e163205594ae9a7ecf4b42150e71
Pranav S Nair <pranavsnair2028@cs.sjcetpalai.ac.in>
2025-07-22 09:51:44 +0530

    Add Files via upload

---
+++ var.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 20;
+    float pi = 3.14;
+    char grade = 'A';
+
+    cout << "Age: " << age << "\nPi: " << pi << "\nGrade: " << grade << endl;
+    return 0;
+}
