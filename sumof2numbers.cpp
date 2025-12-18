75c7d78316989ef1ff8f2696325840cc9189d868
Pranav S Nair <pranavsnair2028@cs.sjcetpalai.ac.in>
2025-12-18 20:12:35 +0530

    Sum of 2 numbers

---
+++ sumof2numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n1,n2,sum;
+    cout << "Enter First Number : ";
+    cin >> n1;
+    cout << "Enter Second Number : ";
+    cin >> n2;
+    sum = n1 + n2;
+    cout << "Sum : " << sum << endl;
+    return 0;
+}
