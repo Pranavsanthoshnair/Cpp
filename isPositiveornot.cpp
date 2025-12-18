4655cf7d137fb98c565ba362469b7f1a325663df
Pranav S Nair <pranavsnair2028@cs.sjcetpalai.ac.in>
2025-12-18 20:19:51 +0530

    Positive or Negative

---
+++ isPositiveornot.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter Number : ";
+    cin >> n;
+    if(n>=0){
+        cout << "Positive" << endl;
+    }
+    else{
+        cout << "Negative" << endl;
+    }
+    return 0;
+}
