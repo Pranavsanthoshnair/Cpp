7fa843bf20aef47415614dde02177511d441094a
Pranav S Nair <pranavsnair2028@cs.sjcetpalai.ac.in>
2025-12-18 19:49:50 +0530

    Input collected

---
+++ fav_num.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int fav_num;
+    cout << "Enter Your Favourite Number : ";
+    cin >> fav_num;
+    cout << "Your Favourite number is : "<< fav_num << endl;
+    return 0;
+}
