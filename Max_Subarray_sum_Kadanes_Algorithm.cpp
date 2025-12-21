5fd00051d917d4344b5abcdaf681beceb144d828
Pranav S Nair <pranavsnair2028@cs.sjcetpalai.ac.in>
2025-12-21 19:29:31 +0530

    Max subarray Sum using kadanes algorithm

---
+++ Max_Subarray_sum_Kadanes_Algorithm.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int main(){
+    int arr[] = {3,-4,5,4,-1,7,-8};
+    int n = 7;
+
+    int currSum = 0;
+    int maxSum = INT_MIN;
+
+    for(int i=0; i<n; i++){
+        currSum += arr[i];
+        maxSum = max(maxSum, currSum);
+
+        if(currSum < 0){
+            currSum = 0;
+        }
+    }
+
+    cout << "Maximum subarray Sum = " << maxSum << endl;
+}
