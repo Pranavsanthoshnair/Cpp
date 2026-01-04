e5217a412c0f74edfc1825c6d837e11f80007763
Pranav S Nair <pranavsnair2028@cs.sjcetpalai.ac.in>
2026-01-04 11:12:29 +0530

    Pointer arithmetic

---
+++ Pointer_arithmetic.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 10;
+    int *p = &a;
+    cout << p << endl;
+    // Pointer arithmetic:
+    // p++ moves the pointer to the next integer location
+    // Since p is an int pointer, it increases by sizeof(int) (usually 4 bytes)
+    p++;
+    cout << p << endl;
+    // p-- moves the pointer back to the previous integer location
+    // Again, movement is by sizeof(int)
+    p--;
+    cout << p << endl;
+    return 0;
+}
