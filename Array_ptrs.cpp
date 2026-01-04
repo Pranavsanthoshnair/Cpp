d68270dea8632843fe3d7347f17981efe520b1ae
Pranav S Nair <pranavsnair2028@cs.sjcetpalai.ac.in>
2026-01-04 11:17:02 +0530

    Array pointer

--- Array_ptrs.cpp
+++ Array_ptrs.cpp
@@ -5,5 +5,9 @@ int main(){
     int arr[]={1,2,3,4,5};   // array pointer is a constant pointer it is not changeable
     cout << arr << endl;     //pointer
     cout << *arr << endl;    // arr points to first index of array
+    cout << *(arr+1) << endl;
+    cout << *(arr+2) << endl;
+    cout << *(arr+3) << endl;
+    cout << *(arr+4) << endl;
     return 0;
 }
